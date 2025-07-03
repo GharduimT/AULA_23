23384b1fd69edfb3febb536938e13637ed00cec6
GharduimT <Gabriel.harduim.ti@gmail.com>
2025-07-03 20:38:41 -0300

    testes rodando com pandas e polars

---
+++ AULA_23/ex_1.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import pandas as pd
+import polars as pl
+
+
+#  obtendo dados
+
+try:
+    ENDERECO_DADOS = r'../dados/'
+
+    hora_inicio = datetime.now()
+
+    print('Carregando...')
+
+    # # #  Pandas
+    # df_janeiro = pd.read_csv(ENDERECO_DADOS + '202501_NovoBolsaFamilia.csv', sep=';', encoding='iso-8859-1')
+    # print(df_janeiro.head())
+    #  Polars
+    df_janeiro = pl.read_csv(ENDERECO_DADOS + '202501_NovoBolsaFamilia.csv', separator=';', encoding='iso-8859-1')
+
+    print(df_janeiro.head())
+
+    #  tempo final
+    hora_final = datetime.now()
+
+    print(f'Tempo de Execução:{hora_final - hora_inicio}')
+except Exception as e:
+    print(f'Erro ao obter dados {e}')
---
+++ AULA_23/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+# #  Pandas
+    # df_janeiro = pd.read_csv(ENDERECO_DADOS + '202501_NovoBolsaFamilia.csv', sep=';', encoding='iso-8859-1')
+    # print(df_janeiro.head())
